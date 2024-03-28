cafc07978f29e606bad8763efbf70805f6fd532d
ukanyarat <150550152+ukanyarat@users.noreply.github.com>
2024-03-28 20:10:24 +0700

    Add files via upload

---
+++ SymbolRows/SymbolRows/SymbolRows.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymbolRows
+{
+    internal class SymbolRows
+    {
+        static void Main(string[] args)
+        {
+            char symbol;
+            int row;
+
+            Console.Write("Please enter symbol : ");
+            symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Please enter number of row : ");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
